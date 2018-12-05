8217063e7e2392c6ded503c6d617d31ce85e3ef0
Christian Paz <christian942010@gmail.com>
2018-12-05 20:04:58 -0300

    Test resdistribuidos en TestRegla_Caso_ - agregada carpeta documentacion

--- test/testing/testReglas.java
+++ test/testing/testReglas.java
@@ -30,6 +30,7 @@ public class testReglas {
     
     @AfterClass
     public static void tearDownClass() {
+        System.gc();
     }
     
     @Before
@@ -38,6 +39,7 @@ public class testReglas {
     
     @After
     public void tearDown() {
+        System.gc();
     }
 
     // TODO add test methods here.
@@ -47,52 +49,78 @@ public class testReglas {
     // public void hello() {}
     
     @Test
-    public void testRegla1(){
+    public void testRegla1Caso1(){
         Regla1 r1 = new Regla1();
         Venta v1 = new Venta();
-        //Caso 2 productos iguales
+        //Caso 2 productos iguales de Panadería
         v1.AgregarDetalle(Negocio.Productos()[0],2);
         assertEquals(25d,r1.RealizarDescuento(v1),0);
-        //Caso 3 productos iguales
+    }
+    
+    @Test
+    public void testRegla1Caso2(){
+        Regla1 r1 = new Regla1();
+        Venta v1 = new Venta();
+        //Caso 3 productos iguales de Panadería
+        v1.AgregarDetalle(Negocio.Productos()[0],2);
         v1.AgregarDetalle(Negocio.Productos()[6],3);
         assertEquals(53d,r1.RealizarDescuento(v1),0);
     }
     
     @Test
-    public void testRegla2(){
+    public void testRegla2Caso1(){
         Regla2 r2 = new Regla2();
         Venta v2 = new Venta();
         //Caso Total >500 y <=1000
         v2.AgregarDetalle(Negocio.Productos()[0],10);
         v2.AgregarDetalle(Negocio.Productos()[1], 1);
-        System.out.println("Resultado 1: "+v2.Total());
-        System.out.println("Descuento 1(3%): "+r2.RealizarDescuento(v2));
-        assertEquals(15.9d,r2.RealizarDescuento(v2),0.01); //Total 530, descuento esperado 15.9
-        //Caso Total >1000 y <=2500
-        v2.AgregarDetalle(Negocio.Productos()[3], 10);
-        System.out.println("Resultado 2: "+v2.Total());
-        System.out.println("Descuento 2(5%): "+r2.RealizarDescuento(v2));
-        assertEquals(56.5d,r2.RealizarDescuento(v2),0.01); //Total 1130, descuento esperado 56.5
-        //Caso Total >2500
-        v2.AgregarDetalle(Negocio.Productos()[5], 100);
-        System.out.println("Resultado 3: "+v2.Total());
-        System.out.println("Descuento 3(6.5%): "+r2.RealizarDescuento(v2));
-        assertEquals(170.95d,r2.RealizarDescuento(v2),0.01); //Total 2630, descuento esperado 170.95
+        //Total 530, descuento esperado 15.9
+        assertEquals(15.9d,r2.RealizarDescuento(v2),0.01);
     }
     
     @Test
-    public void testRegla3(){
+    public void testRegla2Caso2(){
+        Regla2 r2 = new Regla2();
+        Venta v2 = new Venta();
+         //Caso Total >1000 y <=2500
+        v2.AgregarDetalle(Negocio.Productos()[0],10);
+        v2.AgregarDetalle(Negocio.Productos()[1],1);
+        v2.AgregarDetalle(Negocio.Productos()[3],10);
+        //Total 1130, descuento esperado 56.5
+        assertEquals(56.5d,r2.RealizarDescuento(v2),0.01);
+    }
+    
+    @Test
+    public void testRegla2Caso3(){
+        Regla2 r2 = new Regla2();
+        Venta v2 = new Venta();
+        //Caso Total >2500
+        v2.AgregarDetalle(Negocio.Productos()[0],10);
+        v2.AgregarDetalle(Negocio.Productos()[1], 1);
+        v2.AgregarDetalle(Negocio.Productos()[3], 10);
+        v2.AgregarDetalle(Negocio.Productos()[5], 100);
+        //Total 2630, descuento esperado 170.95
+        assertEquals(170.95d,r2.RealizarDescuento(v2),0.01);
+    }
+    
+    @Test
+    public void testRegla3Caso1(){
         Regla3 r3 = new Regla3();
         Venta v3 = new Venta();
         //Caso 2 Unidades Lacteos
         v3.AgregarDetalle(Negocio.Productos()[1], 2);
-        System.out.println("Resultado 1: "+v3.Total());
-        System.out.println("Descuento 1(30% x 2 Unidades): "+r3.RealizarDescuento(v3));//Total 60, descuento esperado 9.0d
+        //Total 60, descuento esperado 9.0d
         assertEquals(9,r3.RealizarDescuento(v3),0.01);
+    }
+    
+    @Test
+    public void testRegla3Caso2(){
+        Regla3 r3 = new Regla3();
+        Venta v3 = new Venta();
         //Caso 5 Unidades Verduras
+        v3.AgregarDetalle(Negocio.Productos()[1], 2);
         v3.AgregarDetalle(Negocio.Productos()[3], 5);
-        System.out.println("Resultado 2: "+v3.Total());
-        System.out.println("Descuento 2(30% x 5 Unidades): "+r3.RealizarDescuento(v3));//Total 360, descuento esperado 81.0d
+        //Total 360, descuento esperado 81.0d
         assertEquals(81,r3.RealizarDescuento(v3),0.01);
     }
     
@@ -102,9 +130,7 @@ public class testReglas {
         Venta v4 = new Venta();
         v4.AgregarDetalle(Negocio.Productos()[0], 10);
         v4.AgregarDetalle(Negocio.Productos()[3], 10);
-        System.out.println("Resultado: "+v4.Total());
-        System.out.println("Descuento en Ventas > $1000 (10% x Producto.Cantidad>3): "
-                +r4.RealizarDescuento(v4)); //Total 1100, descuento esperado 11
+        //Total 1100, descuento esperado 11
         assertEquals(11,r4.RealizarDescuento(v4),0.01);
     }
     
